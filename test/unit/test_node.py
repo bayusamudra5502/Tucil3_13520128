ebd0a23248e9d40989fbb7a5dbee14b9300c37a0
bayusamudra5502 <bayusamudra.55.02.com@gmail.com>
2022-04-01 14:39:02 +0700

    Node berhasil

--- test/unit/test_node.py
+++ test/unit/test_node.py
@@ -1,4 +1,12 @@
 from lib.Node import Node
+from lib.Table import Table
 
-def test_create():
-  pass
+def test_cost():
+  a = [1,2,16,4,5,6,3,8,9,10,7,11,13,14,15,12]
+  b = [1,2,3,4,5,16,6,8,9,10,7,11,13,14,15,12]
+
+  Ta = Table(a)
+  Tb = Table(b)
+
+  assert Node(1, Ta).cost() == 5
+  assert Node(2, Tb).cost() == 6
